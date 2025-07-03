0421204d8815d5ac16dedc965e262638df98d304
DiegoArielAyala <diegoa.ayala@gmail.com>
2025-07-03 08:01:51 +0200

    Rename index.html

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "index.html")
 
 def bike_price_tracker(request):
     return render(request, "bike-price-tracker.html")
